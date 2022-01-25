2382c7d03169c215f5c3a68a4043d89666f4bd03
TKShadowBlade <katanablack85@gmail.com>
2022-01-25 00:56:38 -0500

    profile page startup

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='#'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <a href='https://www.linkedin.com/in/anthony-korneagay-769a503b/'>
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )   
+}
