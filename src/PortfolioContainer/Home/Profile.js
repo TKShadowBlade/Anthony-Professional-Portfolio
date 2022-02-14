775c1622983ef41006117189fc5f0ce9d4c37fda
TKShadowBlade <katanablack85@gmail.com>
2022-02-14 01:31:36 -0500

    tagline and resume link on profile page

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from 'react-typical';
 
 export default function Profile() {
     return (
@@ -18,6 +19,40 @@ export default function Profile() {
                     <span className="primary-text">
                         Hello, I'm <span className="highlighted-text">Anthony</span>
                     </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span claassName='primary-text'>
+                        {" "}
+                        <h1>
+                        <Typical
+                        loop={Infinity}
+                        steps={[
+                            "Website Designer Extraordinaire",
+                            1000,
+                            "Full Stack Developer",
+                            1000,
+                            "MERN Stack Engineer",
+                            1000,
+                            "SQL and MongoDB Databasing",
+                            1000,
+                            "React Developer/Engineer",
+                            1000,
+                        ]}
+                        />
+                        </h1>
+                        <span className='profile-role-tagline'>
+                            Skilled in front and back end development
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        {""}
+                        Please Hire Me!{" "}
+                    </button>
+                    <a href='Anthony Korneagay Jr Updated Resume.docx' download='Anthony Korneagay Jr Updated Resume.docx'>
+                        <button className='btn highlighted-btn'>Get Resume</button>
+                    </a>
                 </div>
                 </div>
             </div>
