6002608bcc19864508dd374b27ede06413bd1eac
TKShadowBlade <katanablack85@gmail.com>
2022-02-18 01:43:37 -0500

    profile.js updates

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -7,12 +7,14 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
+                        <div className='colz-icon'>
                         <a href='#'>
                             <i className='fa fa-facebook-square'></i>
                         </a>
                         <a href='https://www.linkedin.com/in/anthony-korneagay-769a503b/'>
                             <i className='fa fa-linkedin-square'></i>
                         </a>
+                        </div>
                     </div>
                 
                 <div classname="profile-details-name">
@@ -54,6 +56,9 @@ export default function Profile() {
                         <button className='btn highlighted-btn'>Get Resume</button>
                     </a>
                 </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'></div>
                 </div>
             </div>
         </div>
